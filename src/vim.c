02889ff3ae2fbc5bd7771f740add08158591c95a
ctalkobt@ctalkobt.net <ctalkobt@ctalkobt.net>
2025-05-09 08:48:41 -0400

    Get status line looking correct.

--- src/vim.c
+++ src/vim.c
@@ -12,9 +12,21 @@
 
 #define CTRL(kar) ((kar)-'a')
 
+// Temp storage. Only used within a fn "frame". 
+char zTemp[32+1]; 
+
+// Vim is exiting. 
 int exitVim = 0;
+
+// Max x,y coordinates for screen. 
 unsigned char maxX; 
 unsigned char maxY; 
+
+// Current x,y coordinates into text. 
+unsigned char posX, posY; 
+unsigned char maxPosY; 
+
+// Current filename. 
 char zFilename[80+1];   
 
 int mode = MODE_DEFAULT;
@@ -124,9 +136,24 @@ void draw_screen() {
     for(unsigned char x=0;x<maxX;x++) {
         putch('-');
     }
+    // Status line: 
+    // "filename" <count>L, <count>B          x,ypos  n%
+    // "text.txt" 20L, 400B                     5,6 10%
+    // 
     gotoxy(0,maxY-1);
     txtEraseEOS();
-    puts(zFilename); 
+    putchar('\"');puts(zFilename);putchar('\"'); 
+
+    gotoxy(maxX-16,maxY-1);
+    itoa(posX,zTemp,10);
+    puts(zTemp); putchar(',');
+    itoa(posY,zTemp,10);
+    puts(zTemp);     
+
+    gotoxy(maxX-4,maxY-1); 
+    int percent = (maxPosY == 0) ? 0 : ((posY+0.0)/(maxPosY+0.0))*100; 
+    itoa(percent,zTemp,10);
+    puts(zTemp); putchar('%'); 
 }
 
 void edit() {
@@ -151,6 +178,15 @@ void edit() {
         }
     } while (!exitVim);
 };
+
+void cursor_on() {
+    *((unsigned char *)0x1120) = 0; 
+}
+
+void cursor_off() {
+    *((unsigned char *)0x1120) = 16; 
+}
+
 
 int main(void) {
     edit();
