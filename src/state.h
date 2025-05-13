7a901de653836e2773bd9d0b776ad5663b53047a
ctalkobt@ctalkobt.net <ctalkobt@ctalkobt.net>
2025-05-12 21:54:31 -0400

    Segregate existing into different files

---
+++ src/state.h
@@ -0,0 +1,29 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <stdint.h>
+
+#include "cmd.h"
+
+//
+// Edit state. 
+//
+typedef struct tsXYPos {
+    uint8_t xPos; 
+    uint8_t yPos; 
+}; 
+
+
+typedef struct tsState {
+    uint8_t lines;        // Number of lines within the files. 
+    uint8_t lineY;        // Current .Y position within buffer. 
+    uint8_t xPos;         // Current .X position within line. 
+
+    char zFilename[88+1]; // Filename of file being edited. 
+    uint8_t doExit;       // 0 = don't exit, !0 = exiting. 
+    tsXYPos screenStart;  // Start X,Y of render screen. 
+    tsXYPos screenEnd;    // End X,Y of render screen.
+    EditMode editMode;    // Current edit mode. 
+}; 
+
+#endif
--- src/vim.c
+++ src/vim.c
@@ -5,12 +5,10 @@
 #include <mega65/txtio.h>
 #include <conio.h>
 
+#include "state.h"
 #include "cmd.h"
+#include "render.h"
 
-
-
-// Temp storage. Only used within a fn "frame". 
-char zTemp[32+1]; 
 
 // Vim is exiting. 
 int exitVim = 0;
@@ -24,47 +22,16 @@ unsigned char posX, posY;
 unsigned char maxPosY; 
 
 // Current filename. 
-char zFilename[80+1];   
-
 EditMode mode = Default;
 
-void draw_screen() {
-    putch(147); 
-    gotoxy(0,maxY-2); 
-    for(unsigned char x=0;x<maxX;x++) {
-        putch('-');
-    }
-    // Status line: 
-    // "filename" <count>L, <count>B          x,ypos  n%
-    // "text.txt" 20L, 400B                     5,6 10%
-    // 
-    gotoxy(0,maxY-1);
-    txtEraseEOS();
-    putchar('\"');puts(zFilename);putchar('\"'); 
-
-    gotoxy(maxX-16,maxY-1);
-    itoa(posX,zTemp,10);
-    puts(zTemp); putchar(',');
-    itoa(posY,zTemp,10);
-    puts(zTemp);     
-
-    gotoxy(maxX-4,maxY-1); 
-    int percent = (maxPosY == 0) ? 0 : ((posY+0.0)/(maxPosY+0.0))*100; 
-    itoa(percent,zTemp,10);
-    puts(zTemp); putchar('%'); 
-}
-
-void edit() {
-    strcpy(zFilename, "filename.txt"); 
-
+void edit(tsState *psState) {
     // Screen/Border color = black.
     *((char *)53280) = (char) 0;
     *((char *)53281) = (char) 0;
 
-    txtScreen80x50(); 
     maxX=80; maxY=50; 
 
-    draw_screen(); 
+    draw_screen(psState); 
 
     do {
         int kar = getch();
@@ -77,16 +44,22 @@ void edit() {
     } while (!exitVim);
 };
 
-void cursor_on() {
-    *((unsigned char *)0x1120) = 0; 
-}
-
-void cursor_off() {
-    *((unsigned char *)0x1120) = 16; 
-}
-
 
 int main(void) {
-    edit();
+    txtScreen80x50(); 
+
+    tsState *state = malloc(tsState); 
+    state->lines=1;
+    state->lineY=0; 
+    state->xPos=0;
+    strcpy(state->zFilename, "test.txt");
+    state->doExit = 0; 
+    state->screenStart.xPos = 0;
+    state->screenStart.yPos = 0;
+    state->screenEnd.yPos=48;
+    state->screenEnd.xPos=80;
+    state->editMode = Default; 
+
+    edit(state);
     return 0; 
 }
