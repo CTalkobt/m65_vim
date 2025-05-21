521aa3da1b381d05baa5607acdac2c16df81631b
ctalkobt@ctalkobt.net <ctalkobt@ctalkobt.net>
2025-05-21 19:36:13 -0400

    Update for command mode. :q now works.

--- src/vim.c
+++ src/vim.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include <mega65/txtio.h>
 #include <conio.h>
@@ -11,7 +12,7 @@
 
 
 // Vim is exiting. 
-int exitVim = 0;
+bool exitVim = false;
 
 // Max x,y coordinates for screen. 
 unsigned char maxX; 
@@ -20,6 +21,64 @@ unsigned char maxY;
 // Current x,y coordinates into text. 
 unsigned char posX, posY; 
 unsigned char maxPosY; 
+
+extern uint8_t screenX;
+extern uint8_t screenY;
+
+void editCommand(tsState *psState) {
+    static char zCmd[80+1];
+    zCmd[0] = '\0';
+
+    int kar; 
+    gotoxy(0,screenY-1); 
+    txtEraseEOS(); 
+    cursor_on(); 
+    bool escape=false; 
+    do {
+        uint8_t l = strlen(zCmd); 
+        gotoxy(0, screenY-1); 
+        puts(zCmd); 
+        gotoxy(l, screenY-1); 
+        kar = getch();
+        switch (kar) {
+            case 20:    // ins/del
+                if (l>0) {
+                    l--;
+                    zCmd[l]='\0';
+                    putchar(kar); 
+                }
+                escape=false;
+                break;
+            case 27:    // escape
+            case '\n':
+                break;
+            case 22:    // Ctrl-V
+                if(!escape) {
+                    escape=true; 
+                    break;
+                }
+            default:
+                // @@TODO: Filter control codes unless prefixed by Ctrl-V. 
+                // 
+                if(escape) {
+                    zCmd[l]='^';
+                    l++; 
+                }
+                zCmd[l] = kar; 
+                zCmd[l+1] = '\0';
+                putch(kar); 
+                escape=false;
+                break;
+        }
+
+    } while (kar != 27 && kar != '\n');     
+
+    if (strcmp(zCmd, "q") == 0) {
+        exitVim=true; 
+    }        
+
+    psState->editMode = Default; 
+}
 
 void edit(tsState *psState) {
     // Screen/Border color = black.
@@ -40,13 +99,16 @@ void edit(tsState *psState) {
                  if (NULL != cmdFn) {
                     cursor_off(); 
                     cmdFn(psState);
-                     cursor_on(); 
+                    cursor_on(); 
                 } else {
                     // @@TODO:Handle non command keypresses.
-                 }
-                break;
+                }
+                if (psState->editMode == Default)
+                    break;
             case Command:
-                (*((unsigned char*)53280))++;
+//                (*(unsigned char *)53280)++; 
+                editCommand(psState); 
+                draw_screen(psState); 
                 break;
             default:
                 break;
